dd9ebbe5d7a1bef0cf619a1ad9048dde259a159a
pydevsg <ghoshsudipto1129@gmail.com>
2026-05-14 22:40:53 +0100

    Enhance UI with traffic animation and heatmap features

--- sudiviz/utils/branding.py
+++ sudiviz/utils/branding.py
@@ -6,7 +6,7 @@ The version bump to 0.2.0 marks the introduction of dynamic visualization
 """
 from __future__ import annotations
 
-VERSION = "0.3.0"
+VERSION = "0.3.2"
 
 LOGO = r"""
                     _ _       _
