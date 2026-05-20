d25368ead057b2df2f6072a39d86e1034dbd28b5
pydevsg <ghoshsudipto1129@gmail.com>
2026-05-20 21:07:40 +0100

    v0.7.1 - Fix Bandit security scan config

--- sudiviz/utils/branding.py
+++ sudiviz/utils/branding.py
@@ -6,7 +6,7 @@ The version bump to 0.2.0 marks the introduction of dynamic visualization
 """
 from __future__ import annotations
 
-VERSION = "0.7.0"
+VERSION = "0.7.1"
 
 LOGO = r"""
                     _ _       _
