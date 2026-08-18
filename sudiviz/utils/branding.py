8d9bebc99add7f57d8b81320191fee58d74b1a8f
pydevsg <ghoshsudipto1129@gmail.com>
2026-08-18 09:28:54 +0100

    v1.8.0 - sudiviz now available in Go

--- sudiviz/utils/branding.py
+++ sudiviz/utils/branding.py
@@ -6,7 +6,7 @@ The version bump to 0.2.0 marks the introduction of dynamic visualization
 """
 from __future__ import annotations
 
-VERSION = "1.7.5"
+VERSION = "1.8.0"
 
 LOGO = r"""
                     _ _       _
